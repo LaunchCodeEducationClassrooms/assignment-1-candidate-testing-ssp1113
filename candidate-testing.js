a39672086b6bba2746bfb9d4360708cea6ab1a45
SSP1113 <SHAKITAHOUSE@MSN.COM>
2021-02-17 19:39:05 +0000

    ran code

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ['1) Who was the first American woman in space? ', '2) True or false: 5000 meters = 5 kilometers. ', '3) (5 + 3) / 2 * 10 = ? ', '4) Given the array [8,"Orbit", "Trajectory", 45], what entry is at index 2? ', '5) What is the minimum crew size for the ISS? '];
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3) / 2 * 10 = ? ", "Given the array [8,'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -21,7 +21,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
   for (i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = input.question(`${questions[i]}\nYour Answer: `);
+    candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}\nYour Answer: `);
     console.log(`Correct Answer: ${correctAnswers[i]}\n`);
   }
 }
