76b83b7005fa094f69a70d682daad2dd90f5a139
SSP1113 <SHAKITAHOUSE@MSN.COM>
2021-02-18 20:43:29 +0000

    UPDATED OUTPUT

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,26 +8,30 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ['Who was the first American woman in space?', 'True or false: 5000 meters = 5 kilometers.', '(5 + 3) / 2 * 10 = ?', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?', 'What is the minimum crew size for the ISS?'];
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3) / 2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Candidate Name: ");
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}\nYour Answer: `);
-    console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+    candidateAnswers.push(input.question(questions[i]));
   }
 }
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  
+  console.log(`\nCandidate Name: ${candidateName}`);
+
+ for (i=0; i<questions.length; i++) {
+   console.log(`${i+1}) ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
+ }
+
   let totalNumCorrect = 0;
 
       for(i=0; i < correctAnswers.length; i++) {
@@ -37,7 +41,7 @@ function gradeQuiz(candidateAnswers) {
     } 
 
   let grade = totalNumCorrect/questions.length * 100;
-
+  
   console.log(`>>> Overall Grade: ${grade}% (${totalNumCorrect} of ${questions.length} responses correct) <<<`);
 
   if (grade >= 80) {
